99e89fc797fa674c23a832410e5cefe790c1462d
Ramsess Santoyo <ramsesssantoyo66@gmail.com>
2023-06-09 15:36:57 -0600

    creando archivos iniciales

---
+++ src/Apis/Api.jsx
@@ -0,0 +1,43 @@
+
+async function Allproducts() {
+    try{
+        const res = await fetch('https://fakestoreapi.com/products');
+        const data = await res.json();
+        console.log(data);
+        return {data};        
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function Usuarios(){
+    try{
+        const res = await fetch('https://fakestoreapi.com/users')
+        const data = await res.json();
+        return {data};
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function Login(user, password){
+    try{
+        const  res = await fetch('https://fakestoreapi.com/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: user,
+                password: password
+            })
+        })
+        const data = await res.json();
+        return {data};
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+export default {Allproducts, Usuarios, Login}
